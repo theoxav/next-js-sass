167299e665737c01bf5cf14fce53e8f61eecb7ce
theoxav <theoxxav@outlook.fr>
2024-11-19 16:26:15 +0100

    Add dashboard notes page

--- src/app/dashboard/notes/page.tsx
+++ src/app/dashboard/notes/page.tsx
@@ -1,3 +1,21 @@
+import { Button } from '@/components/ui/button';
+import Link from 'next/link';
+
 export default function NotesPage() {
-  return <div>NotesPage</div>;
+  return (
+    <section className="grid items-start gap-y-8">
+      <div className="flex md:items-center md:justify-between flex-col md:flex-row px-2">
+        <div className="grid gap-1">
+          <h2 className="text-3xl uppercase font-black">Notes</h2>
+          <p className="text-lg text-muted-foreground">
+            Ne perdez pas vos idées, prennez des notes.
+          </p>
+          <div className="w-12 bg-white my-2 mx-1 h-[1px]"></div>
+        </div>
+        <Button>
+          <Link href="/dashboard/notes/create">Créer une note</Link>
+        </Button>
+      </div>
+    </section>
+  );
 }
